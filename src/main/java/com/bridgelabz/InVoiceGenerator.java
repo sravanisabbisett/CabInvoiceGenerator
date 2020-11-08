6abd753907283366eea09ba22bda8e4720410df0
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-11-08 11:32:18 +0530

    STARTED:cabInvoiceGenrator

---
+++ src/main/java/com/bridgelabz/InVoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class InVoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("welcome to cab invoice generator");
+    }
+}
---
+++ src/test/java/com/bridgelabz/cabInvoiceTest.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class cabInvoiceTest {
+
+}
