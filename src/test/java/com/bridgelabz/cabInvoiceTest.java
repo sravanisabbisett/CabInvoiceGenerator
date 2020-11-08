44294e35924c0a80d0887ab1e4948ab16c0d1004
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-11-08 11:36:22 +0530

    ADDED:calculateFareMethod to calcuate the fare

--- src/test/java/com/bridgelabz/cabInvoiceTest.java
+++ src/test/java/com/bridgelabz/cabInvoiceTest.java
@@ -5,5 +5,21 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class cabInvoiceTest {
+    InVoiceGenerator inVoiceGenerator;
+    @Before
+    public void setUp() throws Exception {
+        inVoiceGenerator=new InVoiceGenerator();
+    }
 
+    @Test
+    public void givenDistanceAndTime_ShouldReturnFare() {
+        double fare=inVoiceGenerator.calculateFare(5.0,5);
+        Assert.assertEquals(55,fare,0.0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+        double fare=inVoiceGenerator.calculateFare(0.1,1);
+        Assert.assertEquals(5,fare,0.0);
+    }
 }
